585f64387da6ca270c9bdb97c04b61800c618dc2
Marnelas <marnelasbenito@gmail.com>
2019-03-02 15:41:19 +0100

    IronHack prework project advance

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,16 +1,60 @@
 // Rover Object Goes Here
 // ======================
+var ActRover = { direction: "N" };
+var contador = null;
+var x = 0;
+var y = 0;
 
 // ======================
-function turnLeft(rover){
+function turnLeft(rover) {
   console.log("turnLeft was called!");
+  contador++;
 }
 
-function turnRight(rover){
+function turnRight(rover) {
   console.log("turnRight was called!");
+  contador = 2;
 }
 
-function moveForward(rover){
-  console.log("moveForward was called")
+function moveForward(rover) {
+  console.log("moveForward was called");
+  contador = 0;
 }
-
+switch (ActRover.direction) {
+  case "N":
+    if (contador == 1) {
+      ActRover.direction("W");
+      x--;
+    } else if (contador == 2) {
+      ActRover.direction("E");
+      x++;
+    }
+    break;
+  case "E":
+    if (contador == 1) {
+      ActRover.direction("N");
+      y++;
+    } else if (contador == 2) {
+      ActRover.direction("S");
+      y--;
+    }
+    break;
+  case "S":
+    if (contador == 1) {
+      ActRover.direction("E");
+      x++;
+    } else if (contador == 2) {
+      ActRover.direction("W");
+      x--;
+    }
+    break;
+  case "W":
+    if (contador == 1) {
+      ActRover.direction("S");
+      y--;
+    } else if (contador == 2) {
+      ActRover.direction("N");
+      y++;
+    }
+    break;
+}
