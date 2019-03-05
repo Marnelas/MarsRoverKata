c6c7378776b894907868ec92f6c65fdc9f3e1c9c
Marnelas <marnelasbenito@gmail.com>
2019-03-05 14:50:57 +0100

    IronHack prework project advance 2

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,60 +1,98 @@
 // Rover Object Goes Here
 // ======================
-var ActRover = { direction: "N" };
+var rover = { direction: "N" };
 var contador = null;
 var x = 0;
 var y = 0;
 
+
 // ======================
 function turnLeft(rover) {
   console.log("turnLeft was called!");
-  contador++;
+position(1)
+console.log(x + ", " + y)
 }
 
 function turnRight(rover) {
   console.log("turnRight was called!");
-  contador = 2;
+position(2)
 }
 
 function moveForward(rover) {
   console.log("moveForward was called");
-  contador = 0;
+position(0)
 }
-switch (ActRover.direction) {
-  case "N":
-    if (contador == 1) {
-      ActRover.direction("W");
-      x--;
-    } else if (contador == 2) {
-      ActRover.direction("E");
-      x++;
-    }
-    break;
-  case "E":
-    if (contador == 1) {
-      ActRover.direction("N");
-      y++;
-    } else if (contador == 2) {
-      ActRover.direction("S");
-      y--;
-    }
-    break;
-  case "S":
-    if (contador == 1) {
-      ActRover.direction("E");
-      x++;
-    } else if (contador == 2) {
-      ActRover.direction("W");
-      x--;
-    }
-    break;
-  case "W":
-    if (contador == 1) {
-      ActRover.direction("S");
-      y--;
-    } else if (contador == 2) {
-      ActRover.direction("N");
-      y++;
-    }
-    break;
+function position(contador) {
+  switch (rover.direction) {
+    case "N":
+      if (contador == 1) {
+        rover.direction("W");
+        x--;
+        contador = 0;
+      } else if (contador == 2) {
+        rover.direction("E");
+        x++;
+        contador = 0;
+      } else {
+        y++;
+      }
+      break;
+    case "E":
+      if (contador == 1) {
+        rover.direction("N");
+        y++;
+        contador = 0;
+      } else if (contador == 2) {
+        rover.direction("S");
+        y--;
+        contador = 0;
+      } else {
+        x++;
+      }
+      break;
+    case "S":
+      if (contador == 1) {
+        rover.direction("E");
+        contador = 0;
+        x++;
+      } else if (contador == 2) {
+        rover.direction("W");
+        contador = 0;
+        x--;
+      } else {
+        y--;
+      }
+      break;
+    case "W":
+      if (contador == 1) {
+        rover.direction("S");
+        contador = 0;
+        y--;
+      } else if (contador == 2) {
+        rover.direction("N");
+        contador = 0;
+        y++;
+      } else {
+        x--;
+      }
+      break;
+  }
+  function comandlist(list) {
+    var list = ["R","F","F","R","F","F","L","F","R","F","F"];
+for(var i = 0;i < list.length;i++){
+  switch(list[i]){
+case 'R':
+turnRight();
+break;
+case 'L':
+turnLeft();
+break;
+case 'F':
+moveForward();
+break;
+
+  } 
+}
+  }
+ 
 }
